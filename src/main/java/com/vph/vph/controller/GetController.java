bfcac70837d4da90d2ab403227a8a6667ada13f8
bqxtt <1486126243@qq.com>
2021-04-26 14:00:28 +0800

    cors

--- src/main/java/com/vph/vph/controller/GetController.java
+++ src/main/java/com/vph/vph/controller/GetController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "查询接口")
 @RestController
 @RequestMapping("/vph")
+@CrossOrigin
 public class GetController {
 
     @Autowired
