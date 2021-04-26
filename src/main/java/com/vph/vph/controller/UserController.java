e1c636769cacd0294e785c8821b68eb5ccbbc52f
bqxtt <1486126243@qq.com>
2021-04-26 21:24:19 +0800

    cors

--- src/main/java/com/vph/vph/controller/UserController.java
+++ src/main/java/com/vph/vph/controller/UserController.java
@@ -8,10 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author tcg
@@ -20,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "用户")
 @RestController
 @RequestMapping("/vph/user")
+@CrossOrigin
 public class UserController {
     @Autowired
     UserService userService;
